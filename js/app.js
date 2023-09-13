76e9a02c64b222f6043a6863d51b6a91b7051937
Carl-code <arimatea865@gmail.com>
2023-09-13 15:35:23 -0600

    Adding Block Scope Variable

--- js/app.js
+++ js/app.js
@@ -1,55 +1,55 @@
 // Arrays
 
-const arreglo = ['texto', 456.10, false, {propiedad: 'valor'}, [1,2,3]];
+const arreglo = ["texto", 456.1, false, { propiedad: "valor" }, [1, 2, 3]];
 console.log(arreglo);
 
-const amigos = ['Pedro', 'Juan', 'Nepamuseno'];
+const amigos = ["Pedro", "Juan", "Nepamuseno"];
 console.log(amigos[1]);
 
 const colores = [];
-colores[0] = 'Rojo';
-colores[1] = 'Verde';
-colores[2] = 'Blanco';
-colores[3] = 'Amarillo';
+colores[0] = "Rojo";
+colores[1] = "Verde";
+colores[2] = "Blanco";
+colores[3] = "Amarillo";
 
 console.log(colores);
-console.log('El Arreglo Colores tiene: ' + colores.length + ' colores');
+console.log("El Arreglo Colores tiene: " + colores.length + " colores");
 
 // Objetos
 
- const persona = {
-    nombre: 'Carlos',
-    edad: 29,
-    correo: 'correo@correo.com',
-    suscripciones: {
-        web: true,
-        correo: true
-    },
-    coloresFavoritos: ['Azul', 'Morado'],
-    saludo: function(){
-        alert('Hola');
-    }
- };
+const persona = {
+  nombre: "Carlos",
+  edad: 29,
+  correo: "correo@correo.com",
+  suscripciones: {
+    web: true,
+    correo: true,
+  },
+  coloresFavoritos: ["Azul", "Morado"],
+  saludo: function () {
+    alert("Hola");
+  },
+};
 
- console.log(persona.nombre);
- console.log(persona['edad']);
+console.log(persona.nombre);
+console.log(persona["edad"]);
 //  console.log(persona.saludo());
- console.log(persona.coloresFavoritos[1]);
- console.log(persona.suscripciones['web']);
+console.log(persona.coloresFavoritos[1]);
+console.log(persona.suscripciones["web"]);
 
 //  Template literals/strings
 
-const nombre = 'Carlos Ojeda';
+const nombre = "Carlos Ojeda";
 const edad = 29;
-const pais = 'Mexico';
+const pais = "Mexico";
 
 console.log(`La Persona se llama ${nombre} y tiene ${edad}, es de ${pais}`);
 
 // Operador Ternario
 
-const boleto = 'vip';
+const boleto = "vip";
 
-const codigoAcceso = boleto === 'vip' ? 'VIP-123-456' : 'REGULAR-456-789';
+const codigoAcceso = boleto === "vip" ? "VIP-123-456" : "REGULAR-456-789";
 
 console.log(codigoAcceso);
 
@@ -57,20 +57,20 @@ console.log(codigoAcceso);
 
 // Forma #1
 function saludo() {
-    console.log('Hola!');
+  console.log("Hola!");
 }
 
 saludo();
 
 // Forma #2
 const saludo = function () {
-    console.log('Hola desde variable!');
-}
+  console.log("Hola desde variable!");
+};
 
 // Forma #3 - Arrow Function
 const saludo = () => {
-    console.log('Hola!');
-}
+  console.log("Hola!");
+};
 
 saludo();
 
@@ -80,22 +80,22 @@ saludo();
 */
 
 const saludo = (nombre) => {
-    console.log(`Hola ${nombre}!`);
-}
+  console.log(`Hola ${nombre}!`);
+};
 
-saludo('Carlos');
+saludo("Carlos");
 
 // Multiples parametros
 const operacion = (tipo, numero1, numero2) => {
-    if(tipo === 'suma') {
-        console.log(numero1 + numero2)
-    } else if (tipo === 'resta') {
-        console.log(numero1 - numero2)
-    }
-}
+  if (tipo === "suma") {
+    console.log(numero1 + numero2);
+  } else if (tipo === "resta") {
+    console.log(numero1 - numero2);
+  }
+};
 
-operacion('suma', 100, 50);
-operacion('resta', 100, 50);
+operacion("suma", 100, 50);
+operacion("resta", 100, 50);
 
 /* Global Scope o variables locales
     - Son variables declaradas fuera de una funcion.
@@ -103,10 +103,41 @@ operacion('resta', 100, 50);
     - Podemos usar const, let y var.
 */
 
-var apellido = 'Ojeda';
-console.log('Hola ' + apellido);
+var apellido = "Ojeda";
+console.log("Hola " + apellido);
 
 const saludo2 = () => {
-    console.log('Hola ' + apellido);
-}
+  console.log("Hola " + apellido);
+};
 saludo2();
+
+/* Local Scope o Variables Locales
+  - Son las variables declaradas dentro de una funcion.
+  - Solo podemos acceder a ellas desde dentro de la funcion.
+*/
+
+var obtenerNumeroLetras = (nombre) => {
+  var numero = nombre.length;
+  console.log(`${nombre} tiene ${numero} letras`);
+
+  var funcionAnidada = () => {
+    console.log(numero);
+  };
+  funcionAnidada();
+};
+
+obtenerNumeroLetras("Carlos");
+
+/* Block Scope / Alcance de tipo Bloque
+   - Pertenecen a las Variables declaradas con const o let dentro de un bloque {}.
+   - Solo podemos acceder a ellas dentro del bloque.
+*/
+
+const edad1 = 19;
+
+if (edad1 >= 18) {
+  const accesoPermitido = true;
+  if (true) {
+    console.log(accesoPermitido);
+  }
+}
